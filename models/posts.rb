6473d3fb32fde0acaedaf84d2745c8c9ed5bdd29
jamesolsen <james.w.olsen@outlook.com>
2021-06-19 17:30:45 +0930

    ad

--- models/posts.rb
+++ models/posts.rb
@@ -6,7 +6,7 @@ def create_post(title, thumbnail, author, about, main_img)
 
     sql_query = "INSERT INTO project(project_title, project_thumbmail, project_author, project_about, project_main_img) VALUES ('#{title}','#{thumbnail}','#{author}','#{about}','#{main_img}')"
     run_sql(sql_query)
-
+ # SQL bolt
 end 
 
 def delete_post(id)
