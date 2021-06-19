8f4920764fcb6bf7c6f5d85dae4cc0a6752bc8f9
jamesolsen <james.w.olsen@outlook.com>
2021-06-19 12:05:05 +0930

    test

--- helpers/sessions_helper.rb
+++ helpers/sessions_helper.rb
@@ -5,3 +5,11 @@ def is_logged_in?
         return false
     end
 end
+
+def current_user
+    if is_logged_in?
+        return find_user_by_email(session[:user_id])
+    else
+        return nil
+    end
+end
