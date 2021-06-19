7d146eeb5f216e0eecc2f51dc8296fd892ba0e96
jamesolsen <james.w.olsen@outlook.com>
2021-06-19 13:08:35 +0930

    hello

--- main.rb
+++ main.rb
@@ -8,6 +8,7 @@ require_relative 'db/db'
 require_relative 'models/posts'
 require_relative 'models/users'
 require_relative 'helpers/sessions_helper'
+require_relative 'controllers/posts_controller'
 require_relative 'controllers/users_controller'
 require_relative 'controllers/sessions_controller'
 
