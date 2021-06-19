b914e1f16c7c9dfdfb509f206a1651935191ec48
jamesolsen <james.w.olsen@outlook.com>
2021-06-19 14:14:15 +0930

    ad

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -6,7 +6,7 @@ end
 
 post '/create' do
     title = params[:project_title]
-    thumbnail = params[:project_thumbnail]
+    thumbnail = params[:project_thumbmail]
     author = params[:project_author]
     about = params[:project_about]
     main_img = params[:project_main_img]
