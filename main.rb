7472c6627e2390661c614fd66de8ab6a1c70e0b8
jamesolsen <james.w.olsen@outlook.com>
2021-06-19 10:13:21 +0930

    db connect

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ enable :sessoins
 require_relative 'db/db'
 require_relative 'models/posts'
 require_relative 'models/users'
-require_relative 'controllers/users'
+require_relative 'controllers/users_controller'
 require_relative 'controllers/sessions_controller'
 
 get '/' do
