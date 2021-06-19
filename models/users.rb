08542fcd7ed3664552f2c6661b80e9409955dbdb
jamesolsen <james.w.olsen@outlook.com>
2021-06-19 10:10:34 +0930

    db connect

--- models/users.rb
+++ models/users.rb
@@ -4,5 +4,5 @@ def add_user(username, email, password)
 
     password_digest = BCrypt::Password.create(password)
     sql_query = "INSERT INTO users(username, email, password_digest) VALUES('#{username}',#{email}', '#{password_digest}');";
-  run_sql(sql_query
+  run_sql(sql_query)
 end
