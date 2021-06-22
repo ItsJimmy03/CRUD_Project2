18a9b2580fd322cd44c3db23cca03b7a51d11ad0
jamesolsen <james.w.olsen@outlook.com>
2021-06-22 20:24:14 +0930

    ad

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -6,14 +6,14 @@ end
 
 post '/create' do
     id = params[:id]
-    title = params[:project_title]
-    thumbnail = params[:project_thumbmail]
-    author = params[:project_author]
-    about = params[:project_about]
-    main_img = params[:project_main_img]
+    project_title = params[:project_title]
+    project_thumbnail = params[:project_thumbmail]
+    project_author = params[:project_author]
+    project_about = params[:project_about]
+    project_main_img = params[:project_main_img]
     user_id = session[:user_id]
 
-    create_post(title, thumbnail, author, about, main_img, user_id)
+    create_post(project_title, project_thumbnail, project_author, project_about, project_main_img, project_user_id)
 
     redirect '/'
 
--- models/posts.rb
+++ models/posts.rb
@@ -2,12 +2,12 @@ def all_posts()
     run_sql("SELECT * FROM project;")
 end
 
-def create_post(title, thumbnail, author, about, main_img, user_id)
+def create_post(project_title, project_thumbnail, project_author, project_about, project_main_img, user_id)
 
     sql_query = "INSERT INTO project(project_title, project_thumbmail, project_author, project_about, project_main_img, user_id) VALUES ($1, $2, $3, $4, $5, $6)"
-    params = (title, thumbnail, author, about, main_img, user_id)
+    params = (project_title, project_thumbnail, project_author, project_about, project_main_img, user_id)
     run_sql(sql_query, params)
-    
+
  # SQL bolt
 end 
 
